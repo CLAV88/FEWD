66b8c23cf152d7de2a9be95f548862d3b2c05f8e
Calvin B <cbissett88@gmail.com>
2017-11-12 19:05:53 -0500

    added debugger

--- Unit 4 Intermediate JS/Projects/Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
+++ Unit 4 Intermediate JS/Projects/Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
@@ -2,9 +2,18 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+let size, color
+
+$('#sizePicker').click('submit',makeGrid())
 
 function makeGrid() {
-
-// Your code goes here!
-
+    $('#pixel_canvas').addclass('style',"width:100%");
+    //set the height and width variables
+    height = $('#input_height').value
+    width = $("input_width").value
+    color = $('#colorpicker').css()
+    console.log(height)
+    console.log(width)
+    console.log(color)
+    $('#pixel_canvas').attr
 }
