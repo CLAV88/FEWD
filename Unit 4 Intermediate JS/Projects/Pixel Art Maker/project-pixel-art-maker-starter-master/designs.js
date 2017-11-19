e588591d3e2a05446178061e2940af3a38e3bc22
Calvin B <cbissett88@gmail.com>
2017-11-18 23:07:18 -0500

    made alot of changes to js to do color and grid making

--- Unit 4 Intermediate JS/Projects/Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
+++ Unit 4 Intermediate JS/Projects/Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
@@ -2,18 +2,49 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-let size, color
 
-$('#sizePicker').click('submit',makeGrid())
+//n.change(update_n());
+//m.change(update_m());
 
-function makeGrid() {
-    $('#pixel_canvas').addclass('style',"width:100%");
-    //set the height and width variables
-    height = $('#input_height').value
-    width = $("input_width").value
-    color = $('#colorpicker').css()
-    console.log(height)
-    console.log(width)
-    console.log(color)
-    $('#pixel_canvas').attr
+document.getElementById('sizePicker').onsubmit = function() {
+    saveData();
+};
+
+
+function saveData() {
+    event.preventDefault();
+    resetGrid();
+    let height = document.getElementById('input_height').value;
+    let width = document.getElementById('input_width').value;
+
+    makeGrid(height, width);
+};
+
+function resetGrid() {
+    document.getElementById("pixel_canvas").innerHTML = "";
 }
+
+function fillcolor(){
+    $(this).toggleClass('colored');
+    let newfill = $('#colorPicker').val()
+    $('.colored').css('background-color', newfill)
+    if(!$(this).is(".colored")) {
+        $(this).css('background-color',"");
+    }
+};
+
+function makeGrid(height, width) {
+    // Get a reference to the table
+    let n = $('#sizePicker').children('#input_height').val();
+    let m = $('#sizePicker').children('#input_width').val();
+    let tableRef = document.getElementById("pixel_canvas");
+    for(y=0;y<m;y++) {
+       let row = tableRef.insertRow();
+        for(x=0;x<n;x++) {
+            let cell = row.insertCell();
+            cell.addEventListener('click', fillcolor);
+        };
+    };
+};
+
+
