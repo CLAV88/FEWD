a97f677f19ff10fc6cdd7c652a1379faa54f7f5b
Calvin B <cbissett88@gmail.com>
2018-02-19 08:51:16 -0500

    pushing the new files for udacity review

---
+++ Unit 5 Projects/FROGGER/frontend-nanodegree-arcade-game/README.md
@@ -0,0 +1,13 @@
+frontend-nanodegree-arcade-game
+===============================
+
+#Frogger Game Description
+Your implementation must at minimum follow the basic functionality, but you can add additional optional functionality to your game, if you wish.
+
+##Basic Functionality
+
+In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down. The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.
+
+##Instructions to launch game
+
+Included in the index.html are links for the source files that do the work, written in JS.
---
+++ Unit 5 Projects/FROGGER/frontend-nanodegree-arcade-game/js/app.js
@@ -0,0 +1,108 @@
+let n = 3
+// Enemies our player must avoid
+var Enemy = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    let trafficlanes = [225, 140, 60]
+    this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = trafficlanes[Math.floor((Math.random() * trafficlanes.length))];
+    this.row = Math.floor(this.x/83)+1;
+    this.col = Math.floor(this.y/101)+1;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if (this.x > 500) {
+        let trafficlanes = [225, 140, 60];
+        this.x = 0;
+        this.y = trafficlanes[Math.floor((Math.random() * trafficlanes.length))];
+    };
+    this.x += dt * Math.floor((Math.random() * 600) + 1);
+    this.col = Math.floor(this.x/101)+1;
+    this.row = Math.floor(this.y/83)+2;
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.row = 6;
+    this.col = 3;
+    this.y = (((this.row - 1) * 83) -10);
+    this.x = (this.col - 1) * 101;
+};
+
+Player.prototype.update = function () {
+    this.y =(((this.row - 1) * 83) -10);
+    this.x =(this.col -1) * 101;
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (keyCode) {
+    switch(keyCode){
+        case 'left':
+            if (player.col > 1 && player.col < 6){
+                player.col -= 1;
+            }
+            break
+        case 'up':
+            if (player.row > 1 && player.row < 7){
+                player.row -= 1;
+            }
+            break
+        case 'right':
+            if (player.col > 0 && player.col < 5){
+                player.col += 1;
+            }
+            break
+        case 'down':
+            if (player.row > 0 && player.row < 6){
+                player.row += 1;
+            }
+            break
+    };
+};
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+let allEnemies = []
+let player = new Player
+
+let levelEnemies = function (n) {
+    for (n; n > 0; n--){
+        let enemy = new Enemy;
+        enemy.en_index = n;
+        allEnemies.push(enemy);
+    }
+    return allEnemies
+};
+
+levelEnemies(n)
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
