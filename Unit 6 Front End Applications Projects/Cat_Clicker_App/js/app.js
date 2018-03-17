36cac0bd9a332c6fb4345d2d8ed9ab13504f0b0c
Calvin B <cbissett88@gmail.com>
2018-03-17 10:53:56 -0400

    working catclicker phase 3

--- Unit 6 Front End Applications Projects/Cat_Clicker_App/js/app.js
+++ Unit 6 Front End Applications Projects/Cat_Clicker_App/js/app.js
@@ -1,26 +1,26 @@
 //Constructor function for the pics
-function Picture (img_obj) {
-    name = img_obj.alt;
+function Picture (img_obj, i) {
+    this.name = img_obj.alt;
     this.pic_count = 0;
+    this.id = img_obj.id;
+    this.index = i;
 }
 
-Picture.prototype.updateClick = function () {
+Picture.prototype.update = function () {
     this.pic_count += 1;
+    figCaptionArray[this.index].innerText = this.name + "Clicks:" + this.pic_count;
 };
 
-let imgArray = $('.main_img').children().toArray();
+let imgArray = $('.main_img').children('img').toArray();
+let figCaptionArray = $('.main_img').children('figcaption').toArray();
 let catPicArray = [];
 
 for (let i = 0;  i < imgArray.length; i++) {
-    console.log(imgArray[i]);
-    let cat_pic = new Picture(imgArray[i]);
+    let cat_pic = new Picture(imgArray[i], i);
     catPicArray.push(cat_pic);
-    imgArray[i].addEventListener("click", imageInstance(i)(i), false);
+    imgArray[i].addEventListener("click", function () { catPicArray[i].update();}, false);
 }
 
-function imageInstance(i) {
-    catPicArray[i].updateClick();
-}
 
 
 
