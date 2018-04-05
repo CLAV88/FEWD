63501cd08af3fe87a00d4407a7191d1633f93672
Calvin B <cbissett88@gmail.com>
2018-04-05 17:46:59 -0400

    working lesson 1

--- Unit 6 Front End Applications Projects/XHR/course-ajax/lesson-1-async-w-xhr/app.js
+++ Unit 6 Front End Applications Projects/XHR/course-ajax/lesson-1-async-w-xhr/app.js
@@ -3,20 +3,22 @@
     const searchField = document.querySelector('#search-keyword');
     form.addEventListener('submit', function (e) {
         e.preventDefault();
+        const responseContainer = document.querySelector('#response-container');
         responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
+        let searchedForText = searchField.value;
+        const unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 960ee6e399c6b61b9f2886b126caf7bcdc6c7d41bc5c2b9a874bbb028adc887a');
+        unsplashRequest.send();
+
+        function addImage() {
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+            const firstImage = data.results[0];
+
+            htmlContent = `<figure><img src="${firstImage.urls.regular}" alt=${searchedForText}> </figure`;
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
     });
-
 })();
-
-const unsplashRequest = new XMLHttpRequest();
-let searchedForText = 'hippos';
-const responseContainer = document.querySelector('#response-container');
-
-unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-unsplashRequest.onload = addImage;
-unsplashRequest.setRequestHeader('Authorization', 'Client-ID 960ee6e399c6b61b9f2886b126caf7bcdc6c7d41bc5c2b9a874bbb028adc887a');
-unsplashRequest.send();
-function addImage() {
-    console.log('adding image successful');
-}
