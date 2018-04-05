85d85ef4da11ed7b142b30f01da0c671707876c5
Calvin B <cbissett88@gmail.com>
2018-04-05 09:39:57 -0400

    bunch of changes to the json fetch api

--- Unit 6 Front End Applications Projects/XHR/course-ajax/lesson-2-async-w-jQuery/app.js
+++ Unit 6 Front End Applications Projects/XHR/course-ajax/lesson-2-async-w-jQuery/app.js
@@ -10,17 +10,28 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        $.ajax({
+            url: `https://unsplash.com/napi/search?query=${searchedForText}`,
+            type: 'GET',
+            dataType: 'jsonp',
+            headers: {
+                Authorization: 'Client-ID 960ee6e399c6b61b9f2886b126caf7bcdc6c7d41bc5c2b9a874bbb028adc887a'
+            }
+        }).done(addImage);
+        function addImage(data) {
+            let htmlContent = '';
+            if (data && data.results && data.results.length > 1) {
+                const firstImage = data.results[0];
+                htmlContent = `<figure>
+                    <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                </figure>`;
+            } else {
+                htmlContent = '<div class="error-no-image">No images avaiable</div>';
+            }
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
     });
-
-    function addImage(images) {
-        const firstImage = images.results[0];
-
-        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
-                <img src="${firstImage.urls.small}" alt="${searchedForText}">
-                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
-            </figure>`
-        );
-    }
 })();
 
 
--- Unit 6 Front End Applications Projects/XHR/course-ajax/lesson-3-async-w-fetch/app.js
+++ Unit 6 Front End Applications Projects/XHR/course-ajax/lesson-3-async-w-fetch/app.js
@@ -8,5 +8,15 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=hippos`, {
+            mode: 'no-cors',
+            headers: {
+                Authorization: 'Client-ID 960ee6e399c6b61b9f2886b126caf7bcdc6c7d41bc5c2b9a874bbb028adc887a',
+            }
+        }).then(function (response) {
+            debugger;// work with the returned response
+        });
     });
+
+
 })();
